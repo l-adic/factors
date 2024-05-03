daada8b77f9aa30f7aa1cf1d828d4f841ebf96f6
martyall <martyall@protonmail.com>
2024-05-03 16:53:04 -0700

    updated libs but tx is not clearing

--- factors/src/ZK/Factors.hs
+++ factors/src/ZK/Factors.hs
@@ -9,26 +9,25 @@ module ZK.Factors
 where
 
 import Circuit
+import Circuit.Language
 import Data.Field.Galois (GaloisField, Prime)
 import Protolude
 
-factorsE :: (GaloisField f) => ExprM f Wire
-factorsE = do
-  n <- deref <$> freshPublicInput "n"
-  a <- deref <$> freshPrivateInput "a"
-  b <- deref <$> freshPrivateInput "b"
-  let isFactorization = eq n (a `mul` b)
-  compileWithWire (freshPublicInput "out") $ 
-    cond isFactorization (c 1) (c 0)
-
 type Fr = Prime 21888242871839275222246405745257275088548364400416034343698204186575808495617
+
+factorsE :: (GaloisField f, Hashable f) => ExprM f (Var Wire f Bool)
+factorsE = do
+  n <- var_ <$> fieldInput Public "n"
+  a <- var_ <$> fieldInput Private "a"
+  b <- var_ <$> fieldInput Public "b"
+  boolOutput "out" $ eq_ n (a * b)
 
 data Factors f = Factors
   { factorsCircuit :: ArithCircuit f,
     factorsVars :: CircuitVars Text
   }
 
-factors :: forall f. (GaloisField f) => Factors f
+factors :: forall f. (GaloisField f, Hashable f) => Factors f
 factors =
   let BuilderState {..} = snd $ runCircuitBuilder (factorsE @f)
    in Factors
